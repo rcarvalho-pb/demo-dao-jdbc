ee9fe7e47392635fa69561ede02da4bc8e522452
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-01-26 16:21:20 -0300

    FindById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "books");
-		
-		System.out.println(dep);
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dep);
-
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
-import model.dao.impl.SellerDaoImplJDBC;
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoImplJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
