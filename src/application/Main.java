e9dae6524155cc95b1a8d1ccb6bc5ba083b60d1a
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-01-26 15:01:23 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "books");
+		
+		System.out.println(dep);
+
+	}
+
+}
