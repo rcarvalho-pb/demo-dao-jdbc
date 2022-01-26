672867bceae62c4b84ba3541def127db31e3517d
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-01-26 15:09:47 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
@@ -9,7 +12,10 @@ public class Main {
 		Department dep = new Department(1, "books");
 		
 		System.out.println(dep);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dep);
 
+		System.out.println(seller);
 	}
 
 }
